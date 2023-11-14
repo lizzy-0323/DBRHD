781eaecc4c6e8fe3d6d7d40bc757819a50da800e
lazy1 <674194901@qq.com>
2023-11-14 20:03:09 +0800

    feature: update naive network

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 @desc: using multiple method to recognize the handwritten digits
 """
 import os
-import random
+from re import S
+from regex import P
 import torch
 import torch.nn as nn
 import numpy as np
@@ -19,7 +20,7 @@ from torch.utils.data import DataLoader
 # config for cnn
 EPOCH = 5
 BATCH_SIZE = 64
-LR = 0.001
+LR = 0.01
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -68,14 +69,34 @@ def run(methods=None):
             train_data, valid_data, train_labels, valid_labels = TLS(
                 train_data, train_labels, test_size=0.2, random_state=420
             )
-            dnn = DNN(hidden_layer_sizes=(100,), random_state=420).fit(
+            dnn = DNN(hidden_layer_sizes=(500,), random_state=420).fit(
                 train_data, train_labels.ravel()
             )
             # 采用交叉验证
             # print(dnn.score(valid_data, valid_labels))
             acc = dnn.score(test_data, test_labels)
         elif method == "net_naive":
-            net = NET(hidden_layer=100, random_state=420)
+            epochs = EPOCH
+            batch_size = BATCH_SIZE
+            input_size = train_data.shape[1]
+            net = NET(input_size=input_size, hidden_size=500)
+            np.random.seed(420)
+            np.random.shuffle(train_data)
+            np.random.shuffle(train_labels)
+            for epoch in range(epochs):
+                # 按照batch_size进行训练
+                step = 0
+                for i in range(0, input_size, batch_size):
+                    x = train_data[i : i + batch_size]
+                    y = train_labels[i : i + batch_size]
+                    output = net(x)
+                    loss = net.loss(output, y)
+                    net.backward(x, y)
+                    net.update_grad()
+                    net.update_learning_rate()
+                    print(f"Epoch: {epoch}, Step: {step}, Loss: {loss}")
+                    step += 1
+            acc = net.fit(test_data, test_labels)
         elif method == "cnn":
             cnn = CNN()
             train_data = DbrhdDataset(train_data, train_labels)
@@ -86,8 +107,8 @@ def run(methods=None):
             optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
             # 初始化损失函数
             loss_func = nn.CrossEntropyLoss()
-            epoches = EPOCH
-            for epoch in range(epoches):
+            epochs = EPOCH
+            for epoch in range(epochs):
                 cnn.train()
                 for step, data in enumerate(train_loader):
                     x, y = data[0], data[1]
@@ -110,4 +131,4 @@ def run(methods=None):
 
 
 if __name__ == "__main__":
-    run()
+    run(["net_naive"])
--- net.py
+++ net.py
@@ -5,19 +5,197 @@
 """
 import numpy as np
 
+LR = 0.001
+LEARNING_RATE_DECAY = 0.9
 
-class NET(object):
-    def __init__(self, hidden_layer, random_state, optimizer="adam") -> None:
-        pass
 
-    def forward(self):
-        ...
+class FullyConnectedLayer(object):
+    """
+    Fully connected layer
+    """
+
+    def __init__(self, input_size, hidden_size, weight_init_std=0.01) -> None:
+        """
+        :param input_size: the size of input
+        :param hidden_size: the size of hidden layer
+        :param learning_rate: the learning rate
+        """
+        self.params = {}
+        self.params["W"] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params["b"] = np.zeros(hidden_size)
+        self.input = None
+        self.output = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.input = x
+        self.output = np.dot(x, self.params["W"]) + self.params["b"]
+        return self.output
+
+    def backward(self, top_diff):
+        # 计算损失函数对W的导数
+        self.dW = np.dot(self.input.T, top_diff)
+        # 计算损失函数对b的导数
+        self.db = np.sum(top_diff, axis=0, keepdims=True)
+        # 计算损失对于输入的导数
+        dx = np.dot(top_diff, self.params["W"].T)
+        return dx
+
+    def update(self, learning_rate):
+        # print("gradient update")
+        self.params["W"] -= learning_rate * self.dW
+        self.params["b"] -= learning_rate * self.db
+
+
+class SigmoidLayer(object):
+    """
+    sigmoid layer
+    """
+
+    def __init__(self) -> None:
+        self.output = None
+
+    def forward(self, x):
+        self.output = 1 / (1 + np.exp(-x))
+        return self.output
+
+    def backward(self, top_diff):
+        # 计算导数
+        return top_diff * self.output * (1 - self.output)
+
+
+def loss_function(y, y_pred):
+    """
+    cross entropy loss function
+    :param y: the label
+    :param y_pred: the predict label
+    """
+    batch_size = y_pred.shape[0]
+    # 加入1e-7防止log(0)的出现
+    return -np.sum(y * np.log(y_pred + 1e-7)) / batch_size
+
+
+class SoftmaxLayer(object):
+    """
+    softmax layer
+    """
+
+    def __init__(self) -> None:
+        self.loss_func = loss_function
+        self.output = None
+        self.label_one_hot = None
+
+    def forward(self, x):
+        if x.ndim == 2:
+            x = x.T
+            x = x - np.max(x, axis=0)
+            y = np.exp(x) / np.sum(np.exp(x), axis=0)
+            self.output = y.T
+            return self.output
+        x = x - np.max(x)
+        return np.exp(x) / np.sum(np.exp(x))
+
+    def get_loss(self, output, label):
+        batch_size = output.shape[0]
+        # 创建一个one-hot label数组
+        self.label_one_hot = np.zeros_like(output)
+        self.label_one_hot[np.arange(batch_size), label] = 1.0
+        loss = self.loss_func(output, self.label_one_hot)
+        return loss
 
     def backward(self):
-        ...
+        batch_size = self.label_one_hot.shape[0]
+        grad = self.output - self.label_one_hot
+        dx = grad / batch_size
+        return dx
 
-    def fit(self):
-        ...
 
-    def score(self):
-        ...
+def softmax(x):
+    x = x.T
+    x = x - np.max(x, axis=0)
+    y = np.exp(x) / np.sum(np.exp(x), axis=0)
+    return y.T
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def change_one_hot(labels):
+    label_one_hot = np.zeros((len(labels), 10))
+    for i, label in enumerate(labels):
+        label_one_hot[i, int(label)] = 1.0
+    return label_one_hot
+
+
+class NET(object):
+    def __init__(
+        self,
+        input_size=1024,
+        hidden_size=500,
+        output_size=10,
+        weight_init_std=0.01,
+    ) -> None:
+        self.learning_rate = LR
+        self.learning_rate_decay = LEARNING_RATE_DECAY
+        self.params = {}
+        self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params["b1"] = np.zeros(hidden_size)
+        self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params["b2"] = np.zeros(output_size)
+
+    def __call__(self, param):
+        return self.forward(param)
+
+    def forward(self, x):
+        w1, b1 = self.params["W1"], self.params["b1"]
+        w2, b2 = self.params["W2"], self.params["b2"]
+        # 第一个全连接层
+        fc1 = np.dot(x, w1) + b1
+        h1 = sigmoid(fc1)
+        # 第二个全连接层
+        fc2 = np.dot(h1, w2) + b2
+        return softmax(fc2)
+
+    def backward(self, x, label):
+        # forward
+        self.grads = {}
+        w1, b1 = self.params["W1"], self.params["b1"]
+        w2, b2 = self.params["W2"], self.params["b2"]
+        a1 = np.dot(x, w1) + b1
+        h1 = sigmoid(a1)
+        a2 = np.dot(h1, w2) + b2
+        output = softmax(a2)
+        # backward
+        dy = (output - label) / x.shape[0]
+        self.grads["W2"] = np.dot(h1.T, dy)
+        self.grads["b2"] = np.sum(dy, axis=0)
+        da1 = np.dot(dy, w2.T)
+        ha1 = sigmoid(a1)
+        dz1 = (1.0 - ha1) * ha1 * da1
+        self.grads["W1"] = np.dot(x.T, dz1)
+        self.grads["b1"] = np.sum(dz1, axis=0)
+
+    def update_grad(self):
+        self.params["W1"] -= self.learning_rate * self.grads["W1"]
+        self.params["b1"] -= self.learning_rate * self.grads["b1"]
+        self.params["W2"] -= self.learning_rate * self.grads["W2"]
+        self.params["b2"] -= self.learning_rate * self.grads["b2"]
+
+    def fit(self, x, labels):
+        y = self.forward(x)
+        y = np.squeeze(y).astype(int)
+        y = np.argmax(y, axis=1)
+        acc = np.sum(y == labels) / len(labels)
+        return acc
+
+    def loss(self, output, labels):
+        # 将label转换为one-hot
+        labels_one_hot = change_one_hot(labels)
+        # 计算交叉熵损失
+        loss = -np.sum(labels_one_hot * np.log(output + 1e-7)) / len(labels)
+        return loss
+
+    def update_learning_rate(self):
+        self.learning_rate *= self.learning_rate_decay
